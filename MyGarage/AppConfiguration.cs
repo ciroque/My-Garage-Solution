c55b67c3663186e5ad9cf50902a94e95d6e33be4
Steve Wagner <ciroque@live.com>
2024-04-09 15:23:21 -0700

    Remove silly code

--- MyGarage/AppConfiguration.cs
+++ MyGarage/AppConfiguration.cs
@@ -2,16 +2,5 @@
 
 public class AppConfiguration
 {
-    private string _theGarageHost = "http://the-garage.example.com:8080";
-
-    public string TheGarageHost
-    {
-        get
-        {
-            var hostFromEnv = Environment.GetEnvironmentVariable("THE_GARAGE_HOST");
-            return hostFromEnv ?? _theGarageHost;
-        }
-
-        set => _theGarageHost = value;
-    }
+    public readonly string TheGarageHost = "http://the-garage.example.com:8080";
 }
