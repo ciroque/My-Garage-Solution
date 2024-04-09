4ad68e069ddd2f392706a702529f381296806534
Steve Wagner <ciroque@live.com>
2024-04-09 09:30:22 -0700

    Cleaning things up for deployment

--- MyGarage/Program.cs
+++ MyGarage/Program.cs
@@ -3,9 +3,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyGarage;
 using MyGarage.Services;
 
+
+var appConfiguration = new AppConfiguration();
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton(appConfiguration);
 
 // Ensure the HttpClient is available for injection
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
@@ -14,7 +19,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped<ITheGarageClient>(sp =>
 {
     var httpClient = sp.GetRequiredService<HttpClient>();
-    return new TheGarageClient(httpClient);
+    return new TheGarageClient(httpClient, appConfiguration);
 });
 
 await builder.Build().RunAsync();
